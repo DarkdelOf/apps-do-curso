c02c1b534327349961f13cb50ab81afe29005de6
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-10-03 09:50:24 -0300

    fiz a q5.c
    fiz no geany...

---
+++ algoritmos_e_programacao/LISTA DE EXERCÍCIOS 3 – AULA 5/q5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+  setlocale(LC_ALL, "Portuguese");
+  system(chcp 65001 < nul);
+  
+	float idade;
+	
+	printf("Digite a idade do nadador:\n");
+	scanf("%f", &idade);
+	
+	if (idade >= 5 && idade < 8){
+		printf("Nadador categoria Infantil A.");
+	}
+	else{
+		if (idade <= 11){
+			printf("Nadador categoria Infantil B.");
+		}
+		else{
+			if (idade <= 14){
+				printf("Nadador categoria Juvenil A.");
+			}
+			else{
+				if (idade <= 17)
+					printf("Nadador categoria Juvenil B.");
+				else{
+					printf("Nadador categoria Adulto.");
+				}
+			}
+		}
+	}
+}
