d6d990185f46ee948b747c70f8fd7e0aa481dd44
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-10-03 08:33:12 -0300

    Update q2.c

--- algoritmos_e_programacao/LISTA DE EXERCÍCIOS 1 – AULA 3/q2.c
+++ algoritmos_e_programacao/LISTA DE EXERCÍCIOS 1 – AULA 3/q2.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(){
+	int anos, meses, dias;
+	printf("Diga anos, meses e dias de vida, nessa ordem.\n");
+	scanf("%d%d%d", &anos,&meses,&dias);
+	
+	dias = dias+meses*30+anos*365;
+	printf("A pessoas tem ");
+	printf("%d ", dias);
+	printf("dias de vida.");
+}
