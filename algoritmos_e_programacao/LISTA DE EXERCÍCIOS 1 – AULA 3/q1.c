67cd5dcd25b9524e94524656c76ec00661125bc0
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-10-03 08:25:44 -0300

    fiz q1

--- algoritmos_e_programacao/LISTA DE EXERCÍCIOS 1 – AULA 3/q1.c
+++ algoritmos_e_programacao/LISTA DE EXERCÍCIOS 1 – AULA 3/q1.c
@@ -1,1 +1,10 @@
-ola
+#include <stdio.h>
+
+int main(){
+	float L1, L2, A;
+	printf("Insira os valores dos 2 lados do retângulo: ");
+	scanf("%f%f", &L1,&L2);
+	A = L1*L2;
+	printf("A área do retângulo é ");
+	printf("%.2f", A);
+}
