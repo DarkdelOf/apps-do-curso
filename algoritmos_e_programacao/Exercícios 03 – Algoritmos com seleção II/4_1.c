24cfde0921aaefb6a826270a07aac7926d7b50e0
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-10-14 14:35:01 -0300

    Update 4_1.c

--- algoritmos_e_programacao/Exercícios 03 – Algoritmos com seleção II/4_1.c
+++ algoritmos_e_programacao/Exercícios 03 – Algoritmos com seleção II/4_1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(){
+    float n1,n2,media;
+    scanf("%f%f", &n1,&n2);
+    media = (n1+n2)/2;
+    printf("%.1f (média)\n", media);
+    
+    if (media>=7){
+        printf("Aprovado");
+    }
+    else if (media<7 && media>=3){
+        printf("em Exame");
+    }
+    else if (media<3){
+        printf("Reprovado");
+    }
+}
