aed666fed3536d136c4a2f9b4599751b1ddfa0a3
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-10-17 08:22:52 -0300

    Add number reading and positivity check algorithm
    Implement algorithm to read numbers and check positivity.

---
+++ algoritmos_e_programacao/LISTA DE EXERCÍCIOS 4 – AULA 7, 8 e 9/q1.c
@@ -0,0 +1,13 @@
+/*Escreva um algoritmo para repetir a leitura de um número enquanto o valor
+fornecido for diferente de 0. Para cada número fornecido, imprimir se ele é
+NEGATIVO ou POSITIVO. Quando o número 0 for fornecido a repetição deve
+ser encerrada sem imprimir mensagem alguma.*/
+
+#include <stdio.h>
+
+int main(void){
+	float valor;
+	
+	do{ scanf("%f.0", &valor);
+	}while(valor != 0);
+}
