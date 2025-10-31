612873368ccdc221aa600f5fbfb76bd1a9d83164
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-10-31 09:21:36 -0300

    Implement series calculation in q8.c
    Add algorithm to print the first 30 terms of the series 1, 3, 9, 27.

--- algoritmos_e_programacao/LISTA DE EXERCÍCIOS 4 – AULA 7, 8 e 9/q8.c
+++ algoritmos_e_programacao/LISTA DE EXERCÍCIOS 4 – AULA 7, 8 e 9/q8.c
@@ -14,3 +14,19 @@ int main(void){
 		printf("%.0f\n", a);
 	}
 }
+
+// alternativo:
+
+/*Faça um algoritmo que escreva os 30 primeiros termos da série 1,3,9,27..*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	int i;
+	float a;
+	for(i = 0; i < 30; i++){
+		a = pow(3,i);
+		printf("%.0f\n", a);
+	}
+}
