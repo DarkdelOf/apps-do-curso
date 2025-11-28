584fef488b68143c7fb6248b7d6ba188b6782b70
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-11-28 09:25:53 -0300

    Implement array population and printing
    Populate an array with multiples of 10 and print it.

--- algoritmos_e_programacao/Exercícios 14 – Algoritmos sobre vetores I/14_2.c
+++ algoritmos_e_programacao/Exercícios 14 – Algoritmos sobre vetores I/14_2.c
@@ -1,1 +1,14 @@
+/*Preencher um vetor A de 10 elementos(índices de 0 a 9) com os números inteiros
+10,20,30,40,50,...,100. Escrever o vetor A após o seu total preenchimento.*/
 
+#include <stdio.h>
+
+int main(void){
+    int i, A[10];
+    for(i=0;i<10;i++){
+        A[i]=(i+1)*10;
+    }
+    for(i=0;i<10;i++){
+        printf("Índice %d: %d\n", i, A[i]);
+    }
+}
