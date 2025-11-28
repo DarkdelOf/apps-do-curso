83be535b5cb5cbb3ed4182787d94089a9b9bed51
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-11-28 09:20:48 -0300

    Initialize array with 30 and print values
    Fill an array with the integer value 30 and print its contents.

--- algoritmos_e_programacao/Exercícios 14 – Algoritmos sobre vetores I/14_1.c
+++ algoritmos_e_programacao/Exercícios 14 – Algoritmos sobre vetores I/14_1.c
@@ -1,1 +1,14 @@
+/*Preencher um vetor X de 10 elementos(índices de 0 a 9) com o valor inteiro 30.Escrever o vetor X,
+após seu total preenchimento.*/
 
+#include <stdio.h>
+
+int main(void){
+    int vetor[10], i;
+    for(i=0;i<10;i++){
+        vetor[i] = 30;
+    }
+        for(i=0;i<10;i++){
+        printf("Índice %d: %d\n", i,vetor[0]);
+    }
+}
