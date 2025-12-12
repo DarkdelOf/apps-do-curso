5c62f7624f332b53570fca7905694e7427072912
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-12-12 08:04:14 -0300

    Atualizar o 16_1.c

--- algoritmos_e_programacao/Exercícios 16 – Algoritmos sobre matrizes I/16_1.c
+++ algoritmos_e_programacao/Exercícios 16 – Algoritmos sobre matrizes I/16_1.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main(void){
+    int A[5,5], sum, i, j;
+    
+    sum = 0;
+    for(i=0;i<5;i++){
+        sum = sum+A[i,3];
+    }
+    printf("%d", sum);
+    
+    sum = 0;
+    for(i=0;i<5;i++){
+        sum = sum+A[2,i];
+    }
+    printf("%d", sum);
+    
+    sum = 0;
+    for(i=0;i<5;i++){
+        sum = sum+A[i,i];
+    }
+    printf("%d", sum);
+    
+    sum = 0;
+    for(i=0;i<5;i++){
+        sum = sum+A[4-i,i];
+    }
+    printf("%d", sum);
+    
+    sum = 0;
+    for(i=0;i<5;i++){
+        for(j=0;j<5;j++){
+            sum = sum+A[i,j];
+        }
+    printf("%d", sum);
+}
