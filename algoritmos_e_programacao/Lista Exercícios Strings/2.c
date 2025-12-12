2911eb6a45d498542ffce8ba045edc5226f0d904
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-12-12 09:34:21 -0300

    Implement string space trimming function
    Added functionality to remove leading and trailing spaces from a string.

--- algoritmos_e_programacao/Lista Exercícios Strings/2.c
+++ algoritmos_e_programacao/Lista Exercícios Strings/2.c
@@ -4,7 +4,26 @@ string e remove todos os espaços no início e no fim da string.
 Elabore uma função que realiza esta tarefa.*/
 
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
-  
+    char text[100],texttemp[100], i, j;
+    fgets(text, 100, stdin);
+    for(i=0;text[i]!='\0';i++);
+    int c = i;
+    j=0;
+    
+    for(i=0;i<c;i++){
+        if(text[i]==' '){
+        }
+        else{
+            j++;
+            texttemp[j-1] = text[i];
+        }
+    }
+    for(i=0;i<c;i++){
+        text[i] = texttemp[i];
+    }
+    text[j] = '\0';
+    printf("%s", text);
 }
