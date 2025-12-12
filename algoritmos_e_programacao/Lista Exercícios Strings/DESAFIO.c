16b06eee7e8361478d6a8cc0fe93951b05f6db9c
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-12-12 09:38:26 -0300

    Create word counter program structure
    Initialize a word counter program and include necessary headers.

--- algoritmos_e_programacao/Lista Exercícios Strings/DESAFIO.c
+++ algoritmos_e_programacao/Lista Exercícios Strings/DESAFIO.c
@@ -1,1 +1,10 @@
 // fazer um contador de palavras de uma string
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char words[100,100];
+    char text[100];
+    fgets(text,100,stdin);
+}
