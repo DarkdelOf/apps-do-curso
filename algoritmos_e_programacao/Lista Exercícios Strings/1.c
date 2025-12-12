9ac741ece313139127aa4b338600704cb6bc74c0
Darkdel <52863481+DarkdelOf@users.noreply.github.com>
2025-12-12 08:38:29 -0300

    Add string inversion functionality in 1.c
    Implement string inversion function to display reversed string.

--- algoritmos_e_programacao/Lista Exercícios Strings/1.c
+++ algoritmos_e_programacao/Lista Exercícios Strings/1.c
@@ -3,3 +3,17 @@ seguida, faça uma função que inverta e exiba-o na tela.
 Exemplo: Para a string "Bom dia" o programa imprime "aid
 moB" Obs: O objetivo é alterar os caracteres na string, não é
 imprimir ao contrário!*/
+
+#include <stdio.h>
+
+int main(void){
+    
+    int i,c;
+    char nometemp[100];
+    char nome[100] = "Bom dia";
+    for(c=0;nome[c]!='\0';c++);
+    for(i=0;i<c;i++){
+        nometemp[(c-1)-i] = nome[i];
+    }
+    printf("%s", nometemp);
+}
